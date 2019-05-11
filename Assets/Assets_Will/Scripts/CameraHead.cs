4e8c0728b939a516260dfe2d50c61c844c903c68
William Farah-Lajoie <william.farah-lajoie.1@ens.etsmtl.ca>
2019-05-11 13:20:54 -0400

    Trauma Scene

---
+++ Assets/Assets_Will/Scripts/CameraHead.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+
+public class CameraHead : MonoBehaviour
+{
+    public float speedRotation = 5;
+    public float speedMouvement = 5;
+    public float speedThickness = 0.1f;
+
+    bool startMotion = false;
+
+    GameObject head;
+    GameObject panel;
+    Image image;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        head = GameObject.Find("PositionHead");/*.GetComponentsInChildren<GameObject>().First(x => x.name == "position");*/
+        panel = GameObject.Find("Panel");
+        image = panel.GetComponent<Image>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(startMotion)
+        {
+
+            Quaternion targetRotation = Quaternion.LookRotation(head.transform.position - transform.position);
+
+            // Smoothly rotate towards the target point.
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speedRotation * Time.deltaTime);
+
+            if(targetRotation == transform.rotation)
+            {
+                Vector3 destination = head.transform.position;
+                transform.position = Vector3.Lerp(transform.position, destination, speedMouvement * Time.deltaTime);
+                if(image.color.a < 1)
+                    image.color = new Color(image.color.r, image.color.b, image.color.g, image.color.a + speedThickness);
+            }
+        }
+    }
+
+    public void StartMouvement()
+    {
+        startMotion = true;
+    }
+}
---
+++ Assets/Assets_Will/Scripts/HeadForce.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadForce : MonoBehaviour
+{
+    public float force = 5;
+    Rigidbody rb;
+    CameraHead ch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        StartCoroutine("TimerHeadForce");
+
+        ch = Camera.main.GetComponent<CameraHead>();
+    }
+
+    IEnumerator TimerHeadForce()
+    {
+        yield return new WaitForSeconds(1);
+        rb.useGravity = true;
+
+        float x = Random.value;
+        float y = Random.value;
+        float z = Random.value;
+        rb.AddForce(new Vector3(x,y,z).normalized * force, ForceMode.Impulse);
+
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.name == "Ground")
+        {
+            rb.isKinematic = true;
+            ch.StartMouvement();
+        }
+    }
+
+}
