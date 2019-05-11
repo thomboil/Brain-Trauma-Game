1b8be2dbbab1270af183afb05e139546413c527d
William Farah-Lajoie <william.farah-lajoie.1@ens.etsmtl.ca>
2019-05-11 00:47:51 -0400

    Generation ennemi

---
+++ Assets/Assets_Will/Scripts/Ennemi.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ennemi : MonoBehaviour
+{
+
+    int vie = 2;
+    int vitesse = 1;
+    int damage = 1;
+
+    Vector3 direction;
+    Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = transform.forward;
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(direction) * vitesse * Time.deltaTime);
+
+        //rotation
+        if (Random.value == 0)
+            transform.Rotate(0, Random.Range(0f, 5f), 0);
+        else
+            transform.Rotate(0, Random.Range(-5f, 0f), 0);
+
+    }
+}
---
+++ Assets/Assets_Will/Scripts/GenerationEnnemi.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerationEnnemi : MonoBehaviour
+{
+
+    public int nbVagues = 5;
+    public float nextVague = 4;
+
+    public GameObject Ennemi;
+    GameObject planet;
+
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        SpawnEnnemi();
+        StartCoroutine("TimerNextVague");
+        GetPlanet();
+    }
+
+    IEnumerable TimerNextVague()
+    {
+        yield return new WaitForSeconds(nextVague);
+        SpawnEnnemi();
+        StartCoroutine("TimerNextVague");
+
+    }
+
+    public void GetPlanet()
+    {
+        planet = GameObject.FindGameObjectWithTag("Planet");
+    }
+
+    int distanceSpawn = 10;
+
+    public void SpawnEnnemi()
+    {
+        float scale = planet.transform.localScale.x + distanceSpawn;
+        float x = Random.Range(-scale, scale);
+        float z = Random.Range(-scale, scale);
+
+        float hypo = Mathf.Sqrt(x * x + z * z);
+        float y = Mathf.Sqrt(scale * scale - hypo * hypo);
+
+        Vector3 positionSpawn = new Vector3(x, y, z);
+
+        GameObject ennemi = Instantiate(Ennemi, positionSpawn, Quaternion.identity);
+    }
+}
