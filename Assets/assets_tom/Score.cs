79c1f3da85f5c0451869eb3917dd434d705a5c6e
thomboil <thomas.boily98@gmail.com>
2019-05-10 23:25:19 -0400

    Essaie 1
    on fait juste essayer

---
+++ Assets/assets_tom/Score.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    public static int score; //A level will be 1000 pts
+    public Text scoreTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        scoreTxt.text = score.ToString();   //Text starts at zero
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreTxt.text = score.ToString();
+    }
+}
---
+++ Assets/assets_tom/healthbar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthbar : MonoBehaviour
+{
+
+    public Image bar;
+    public float fillAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fillAmount = Score.score / 100; 
+        bar.fillAmount = fillAmount;
+        
+    }
+}
