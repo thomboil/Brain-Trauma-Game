e8ef348fce10eee4307595d81f26814465a7977b
thomboil <thomas.boily98@gmail.com>
2019-05-11 18:36:51 -0400

    Pourcentage
    adndnosdfnvojsnvjksdfnvkjsdbkjSDBVHBSDVCVHBZXDJHCVBZKDHCBKhsdzBCVKHZDB W

--- Assets/assets_tom/Scripts/pourcentage.cs
+++ Assets/assets_tom/Scripts/pourcentage.cs
@@ -8,17 +8,24 @@ public class pourcentage : MonoBehaviour
 {
 
     public Text pourcentageTxt;
+    public GameObject audioBad;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioBad.GetComponent<AudioSource>().volume = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        //Calcul et affichage de la sante de la planete
         int pourcentage = (1 - (Ennemi.nbEnnemi / PlanetColor.nbMaxEnnemi))*100;
-        pourcentageTxt.text = pourcentage.ToString();
+        pourcentageTxt.text = pourcentage.ToString() + "%";
+
+        //Le son de peur augmente quand la planete est infectee
+        audioBad.GetComponent<AudioSource>().volume = Ennemi.nbEnnemi / PlanetColor.nbMaxEnnemi;
+
     }
 }
