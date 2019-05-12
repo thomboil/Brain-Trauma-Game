b914a4e6675c509bdf0971893f4947ac57b02091
William Farah-Lajoie <william.farah-lajoie.1@ens.etsmtl.ca>
2019-05-11 21:57:33 -0400

    Touch control

--- Assets/Assets_Will/Scripts/PlayerMouvement.cs
+++ Assets/Assets_Will/Scripts/PlayerMouvement.cs
@@ -22,6 +22,7 @@ public class PlayerMouvement : MonoBehaviour
     {
         direction = new Vector3(0, 0, Input.GetAxis("Vertical")).normalized;
         rotation = Input.GetAxis("Horizontal");
+        Touch();
     }
 
     void FixedUpdate()
@@ -29,5 +30,39 @@ public class PlayerMouvement : MonoBehaviour
         //rb.AddForce(direction * speed);
         GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(direction) * speedMouvement * Time.deltaTime);
         transform.Rotate(0, rotation* speedRotation, 0);
+    }
+
+    private Vector2 touchOrigin;
+
+    private void Touch()
+    {
+        if (Input.touchCount > 0)
+        {
+            //Store the first touch detected.
+            Touch myTouch = Input.touches[0];
+
+            //Check if the phase of that touch equals Began
+            if (myTouch.phase == TouchPhase.Began)
+            {
+                //If so, set touchOrigin to the position of that touch
+                touchOrigin = myTouch.position;
+            }
+
+            //If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
+            else if (myTouch.phase == TouchPhase.Moved)
+            {
+                float x = myTouch.position.x - touchOrigin.x;
+
+                x = x > 0 ? 1 : -1;
+                direction = new Vector3(0, 0, x).normalized;
+
+            }
+
+            else if(myTouch.phase == TouchPhase.Stationary)
+            {
+                touchOrigin = myTouch.position;
+
+            }
+        }
     }
 }
